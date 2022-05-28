678e33914eab70ed14d8b59c1c7b22f3baab3919
Cristina <maria.cristina217@yahoo.com>
2022-05-28 22:56:39 +0300

    getters&setters

--- models/Products.py
+++ models/Products.py
@@ -3,4 +3,58 @@ from db.DbHandle import DbHandle
 class Products:
 
     def __init__(self,product_name,product_category,product_price,guaranty,product_details,product_image):
-        pass
+        self._product_name = product_name
+        self._product_category = product_category
+        self._product_price = product_price
+        self._guaranty = guaranty
+        self._product_details = product_details
+        self._product_image = product_image
+
+    @property
+    def product_name(self):
+        return self._product_name
+
+    @product_name.setter
+    def product_name(self,product_name):
+        self._product_name = product_name
+
+    @property
+    def product_category(self):
+        return self._product_category
+
+    @product_category.setter
+    def product_category(self,product_category):
+        self._product_category = product_category
+
+    @property
+    def product_price(self):
+        return self._product_price
+
+    @product_price.setter
+    def product_price(self,product_price):
+        self._product_price = product_price
+
+    @property
+    def guaranty(self):
+        return self._guaranty
+
+    @guaranty.setter
+    def guaranty(self,guaranty):
+        self._guaranty = guaranty
+
+    @property
+    def product_details(self):
+        return self._product_details
+
+    @product_details.setter
+    def product_details(self, product_details):
+        self._product_details = product_details
+
+    @property
+    def product_image(self):
+        return self._product_image
+
+    @product_image.setter
+    def product_image(self,product_image):
+        self._product_image = product_image                                                    
+
--- models/Users.py
+++ models/Users.py
@@ -23,4 +23,85 @@ class Users:
     def cid(self,cid):
         self._cid = cid
 
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self,email):
+        self._email = email
+
+    @property
+    def role(self):
+        return self._role
+
+    @role.setter
+    def role(self,role):
+        self._role = role
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self,first_name):
+        self._first_name = first_name
+
+    @property    
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self,last_name):
+        self._last_name = last_name
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self,password):
+        self._password = password
+
+    @property
+    def registered_date(self):
+        return self._registered_date
+
+    @registered_date.setter
+    def registered_date(self,registered_date):
+        self._registered_date = registered_date
+
+    @property
+    def country(self):
+        return self._country
+
+    @country.setter
+    def country(self,country):
+        self._country = country
+
+    @property
+    def phone_number(self):
+        return self._phone_number
+
+    @phone_number.setter
+    def phone_number(self,phone_number):
+        self._phone_number = phone_number
+
+    @property
+    def vat(self):
+        return self._vat
+
+    @vat.setter
+    def vat(self,vat):
+        self._vat = vat
+
+    @property
+    def deposit(self):
+        return self._deposit
+
+    @deposit.setter
+    def deposit(self,deposit):
+        self._deposit = deposit                            
+
+
     
