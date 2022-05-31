9da52cf9d5ea5713a89e389c5805e91e54e286bb
Cristina <maria.cristina217@yahoo.com>
2022-06-01 00:18:58 +0300

    str&serialize

--- models/Products.py
+++ models/Products.py
@@ -2,13 +2,22 @@ from db.DbHandle import DbHandle
 
 class Products:
 
-    def __init__(self,product_name,product_category,product_price,guaranty,product_details,product_image):
+    def __init__(self,pid,product_name,product_category,product_price,guaranty,product_details,product_image):
+        self._pid = pid
         self._product_name = product_name
         self._product_category = product_category
         self._product_price = product_price
         self._guaranty = guaranty
         self._product_details = product_details
         self._product_image = product_image
+
+    @property
+    def pid(self):
+        return self._pid
+
+    @pid.setter
+    def pid(self,pid):
+        self._pid = pid
 
     @property
     def product_name(self):
@@ -58,3 +67,19 @@ class Products:
     def product_image(self,product_image):
         self._product_image = product_image                                                    
 
+    def __str__(self) -> str:
+        return "Product id : {}".format(self.pid)
+
+    def __repr__(self) -> str:
+        return "Product id : {}".format(self.pid)
+
+    def serialize(self):
+        return {
+            "pid" : self.pid,
+            "product_name" : self.product_name,
+            "product_category" : self.product_category,
+            "product_price" : self.product_price,
+            "guaranty" : self.guaranty,
+            "product_details" : self.product_details,
+            "product_image" : self.product_image
+        }    
