00e4ef58e09f5a3cccd71599361b424e769e91a3
Sebastian Mihalache <sebastian.mihalache@devnest.ro>
2022-06-04 18:44:11 +0300

    Quick fix

--- products/products_list.py
+++ products/products_list.py
@@ -1,22 +1,23 @@
 from math import prod
-from application.app import app 
+from application.app import app
 from libs.token import check_auth
 from flask import redirect, render_template, request
 from products.ProductRepository import ProductRepository
 
-@app.route("/products", methods = ["GET","POST"])
+
+@app.route("/products", methods=["GET", "POST"])
 @check_auth
 def products_list(user):
     product_repo = ProductRepository()
     if request.method == "POST":
         print(request.form)
         for _ in range(int(request.form['quantity'])):
-            product_repo.add_to_cart(int(request.form['product_id']),user.cid)
+            product_repo.add_to_cart(int(request.form['product_id']), user.cid)
         return redirect(request.url)
     products = product_repo.get_all()
     by_category = request.args.get("category")
     products_by_cat = product_repo.sort_by_category(by_category)
     if products_by_cat:
-        return render_template("products_list.html",user = user.serialize(), products = products_by_cat)
+        return render_template("products_list.html", user=user.serialize(), products=products_by_cat, sorty_by=by_category)
     else:
-        return render_template("products_list.html",user = user.serialize(), products = products)
+        return render_template("products_list.html", user=user.serialize(), products=products, sorty_by="ALL")
